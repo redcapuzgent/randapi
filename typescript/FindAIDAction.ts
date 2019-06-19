dcc63171fd6cf9e3611ad65c34ecd6d0e4ae2604
Bram Gadeyne <bram.gadeyne@ugent.be>
2019-06-19 07:18:21 +0200

    add docs

--- typescript/FindAIDAction.ts
+++ typescript/FindAIDAction.ts
@@ -1,6 +1,11 @@
 import {RandApiAction} from "./RandApiAction";
 
 export class FindAIDAction extends RandApiAction{
+    /**
+     *
+     * @param parameters The recordid
+     * @param token
+     */
     constructor(public parameters:string, public token:string){
         super("findAID",token,parameters);
     }
